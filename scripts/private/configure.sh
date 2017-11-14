4eb91d565ac38913af2ffa195fdf124d13ac2296
Shanmukha <shanmukhananda@gmail.com>
2017-11-14 16:25:11 +0530

    moved xrdp configuration to script

--- scripts/private/configure.sh
+++ scripts/private/configure.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-useradd -ms /bin/bash desktop
 echo 'pgrep -U $(id -u) lxsession | grep -v ^$_LXSESSION_PID | xargs --no-run-if-empty kill' > /bin/lxcleanup.sh
 chmod +x /bin/lxcleanup.sh
 echo '@lxcleanup.sh' >> /etc/xdg/lxsession/LXDE/autostart
-echo "desktop:desktop" | chpasswd
+echo "root:root" | chpasswd
+xrdp-keygen xrdp auto
