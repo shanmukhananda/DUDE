c4d168e9416ec4aeb3301c2cd6adf4d9585e72f1
Shanmukha <shanmukhananda@gmail.com>
2017-11-14 12:22:01 +0530

    use lxde env for rdp

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,15 +8,4 @@ fi
 
 script_dir=$(realpath $(dirname $0))
 ${script_dir}/private/install-from-apt.sh
-
-configure() {
-    useradd -ms /bin/bash desktop
-    sed -i '/TerminalServerUsers/d' /etc/xrdp/sesman.ini
-    sed -i '/TerminalServerAdmins/d' /etc/xrdp/sesman.ini
-    xrdp-keygen xrdp auto
-    echo "desktop:desktop" | chpasswd
-    touch /home/desktop/.xsession
-    echo "mate-session" > /home/desktop/.xsession
-}
-
-configure
+${script_dir}/private/configure.sh
