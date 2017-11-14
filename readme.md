1d087f1912824cfbda2ba9830d7197640539651e
Shanmukha <shanmukhananda@gmail.com>
2017-11-14 10:05:54 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# DUDE (Docker Ubuntu Desktop Environment)
+
+[![Build Status](https://travis-ci.org/shanmukhananda/DUDE.svg?branch=master)](https://travis-ci.org/shanmukhananda/DUDE)
+
+
+Refer [Wiki](https://github.com/shanmukhananda/DUDE/wiki)
---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! "${EUID}" = "0" ]
+then
+   echo "ERROR: ${0} must be run as root" 
+   exit 1
+fi
+
+script_dir=$(realpath $(dirname $0))
+${script_dir}/private/install-from-apt.sh
+
+configure() {
+    sed -i '/TerminalServerUsers/d' /etc/xrdp/sesman.ini
+    sed -i '/TerminalServerAdmins/d' /etc/xrdp/sesman.ini
+    xrdp-keygen xrdp auto
+    echo "root:root" | chpasswd
+    echo "mate-session" > /home/root/.xsession
+}
+
+configure
